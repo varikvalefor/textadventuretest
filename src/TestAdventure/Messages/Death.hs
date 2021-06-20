8793fd382280bcfa13084cd33f158e4bf3cf50ab
Varik Valefor <varikvalefor@aol.com>
2021-06-20 05:41:55 -0400

    TestAdventure/Messages/Death.hs: A semicolon is added.

--- src/TestAdventure/Messages/Death.hs
+++ src/TestAdventure/Messages/Death.hs
@@ -10,7 +10,7 @@ spontComb = "You spontaneously combust, thereby losing the game and making yours
 answerAff :: String;
 answerAff ="You answer no one in the affirmative.\nNo one responds with a swift punch to the sternum.\n\"Gah!\"\nYou collapse to the ground, slowly melting into cheese.\nJust before dying, you awaken from your wack-ass dream and remember that you are actually the great BRIAN W. KERNIGHAN.";
 
-winMsg :: String
+winMsg :: String;
 winMsg = "YOU HAVE ACCOMPLISHED\nTHE MISSION.\nYOU ARE THE VERY PREVAILER\nTHAT PROTECT RIGHT\nAND JUSTICE.\nI WOULD EXPRESS MY SINCERE.\nTHANKS TO YOU.\n\nTAKE GOOD REST !\n\n\tGENERAL KAWASAKI";
 
 -- | standard is printed to the terminal if the player dies for some
