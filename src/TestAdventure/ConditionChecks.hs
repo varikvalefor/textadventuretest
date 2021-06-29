e2e45339d1cd85613358793a49f7b88fbffbed2b
Varik Valefor <varikvalefor@aol.com>
2021-06-28 22:29:47 -0400

    ConditionChecks.hs: The documentation's markup improves.

--- src/TestAdventure/ConditionChecks.hs
+++ src/TestAdventure/ConditionChecks.hs
@@ -2,47 +2,47 @@ module TestAdventure.ConditionChecks where
 import Data.Char (toUpper);
 import Data.List.Split (splitOn);
 
--- | For all 'String' k, isSuicide k iff k demands that the player
+-- | For all @'String' k@, @isSuicide k@ iff @k@ demands that the player
 -- character commits suicide.
 isSuicide :: String -> Bool;
 isSuicide = (`elem` ["KILL SELF", "SUICIDE", "EXPLODE", "KABOOM", "DIVIDE BY ZERO"]) . map toUpper;
 
--- | For all 'String' k, isAffirmative k iff k is an affirmative
+-- | For all @'String' k@, @isAffirmative k@ iff @k@ is an affirmative
 -- response.
 isAffirmative :: String -> Bool;
 isAffirmative k = or [k == "YES"];
 
--- | For all 'String' k, isSecretWord k iff k equals the secret word...
--- technically, the secret phrase.
+-- | For all @'String' k@, @isSecretWord k@ iff @k@ equals the secret
+-- word... technically, the secret phrase.
 isSecretWord :: String -> Bool;
 isSecretWord = (== "HAM AND SWISS ON RYE");
 
--- | For all 'String' k, isCheckBag k iff k demands that the contents
--- of the player character's inventory are listed.
+-- | For all @'String' k2, @isCheckBag k@ iff @k@ demands that the
+-- contents of the player character's inventory are listed.
 isCheckBag :: String -> Bool;
 isCheckBag k = map toUpper k `elem` ["LIST INVENTORY", "INVENTORY"];
 
--- | For all 'String' k, @isObsSurround k@ iff k demands that the player
--- character's surroundings are listed.
+-- | For all @'String' k@, @isObsSurround k@ iff @k@ demands that the
+-- player character's surroundings are listed.
 isObsSurround :: String -> Bool;
 isObsSurround = (`elem` ["LOOK AROUND YOU"]) . map toUpper;
 
--- | For all 'String' k, @isDemolish k@ iff k demands that the player
--- character smashes up something.
+-- | For all @'String' k@, @isDemolish k@ iff @k@ demands that the
+-- player character smashes up something.
 isDemolish :: String -> Bool;
 isDemolish = (`elem` ["SMASH"]) . (!! 0) . splitOn " " . map toUpper;
 
--- For all 'String' k, @isGo k@ iff k demands that the player moves
--- somewhere.  k need not actually be a followable instruction.
+-- For all @'String' k@, @isGo k@ iff @k@ demands that the player moves
+-- somewhere.  @k@ need not actually be a followable instruction.
 isGo :: String -> Bool;
 isGo = (== "GO") . (!! 0) . words;
 
--- | For all 'String' k, @isFlip k@ iff k demands that the player
--- characterflips something, e.g., the crappy living room table.
+-- | For all @'String' k@, @isFlip k@ iff @k@ demands that the player
+-- character flips something, e.g., the crappy living room table.
 isFlip :: String -> Bool;
 isFlip = (== "FLIP") . (!!0) . words;
 
--- | For all 'String' k, @isClean k@ iff k demands that the player
+-- | For all @'String' k@, @isClean k@ iff @k@ demands that the player
 -- character cleans up some mess, e.g., the debris of the living
 -- room table.
 isClean :: String -> Bool;
