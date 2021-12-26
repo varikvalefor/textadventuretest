3be25883f3a88bff00d10943a16b392153397aad
Varik Valefor <varikvalefor@aol.com>
2021-12-26 14:55:35 -0500

    Documentation is improved.

--- src/VVXtAdventure/Base.hs
+++ src/VVXtAdventure/Base.hs
@@ -1,28 +1,32 @@
 module VVXtAdventure.Base where
--- | GameData contains game data, e.g., the player's name, the contents
--- of the player's inventory, whether or not a question is currently
--- asked, and whether or not the player is become a corpse.
+-- | 'GameData' contains game data, e.g., the player's name, the
+-- contents of the player's inventory, whether or not a question
+-- is currently asked, and whether or not the player is become a corpse.
 data GameData = GameData {
-  -- | For all 'GameData' k, playerName k equals the name of the player
-  -- character, NOT the name of the player, of k.
+  -- | @playerName k@ equals the name of the player character, NOT the
+  -- name of the player, of @k@.
   playerName :: CharName,
-  -- | For all 'GameData' k, playerName k equals the contents of the
-  -- inventory of the player character of k.
-  -- The order of inventory is arbitrary.
+  -- | @playerName k@ equals the contents of the inventory of the player
+  -- character of @k@.
+  --
+  -- The order of @inventory@ is arbitrary.
   inventory :: [Item],
-  -- | For all 'GameData' k, questionYNExists k iff a question is
-  -- currently asked such that the player responds.
+  -- | @questionYNExists k@ iff a question is currently asked such that
+  -- the player responds.
   questionYNExists :: Bool,
-  -- | For all 'GameData' k, secretWordNums k denotes the number of
-  -- occurrences of the secret word, which is generally actually a
-  -- secret phrase, but whatever.
+  -- | @secretWordNums k@ denotes the number of occurrences of the
+  -- secret word, which is generally actually a secret phrase, but
+  --- whatever.
   secretWordNums :: Integer,
-  -- | For all 'GameData' k, status k equals the status of the player
-  -- character of k.  @status k == Alive@ iff the player character of
-  -- k is alive.  @status k == Dead@ iff the player character of k is
+  -- | @status k@ equals the status of the player character of @k@.
+  -- @status k == Alive@ iff the player character of k is alive.
+  -- @status k == Dead@ iff the player character of k is
   -- dead.
   status :: State,
   -- | To what extent is the living room table smashed up?
+  --
+  -- This value is incremented by one (1) whenever the living room
+  -- table is smashed.
   lrTableSmashedness :: Integer,
   -- | Is the table in the living room flipped upside-down?
   lrTableFlipped :: Bool,
@@ -31,9 +35,15 @@ data GameData = GameData {
   lrTableDebrisPresent :: Bool,
   -- | What is the player character's current location?
   currentRoom :: Room,
-  -- | How smashed is the map?
+  -- | How smashed is the mop?
+  --
+  -- This value equals the number of smashings which the mop has
+  -- received.
   mopSmashedness :: Integer,
   -- | How smashed is the broom?
+  --
+  -- This value equals the number of smashings which the broom has
+  -- received.
   broomSmashedness :: Integer,
   -- | What, if anything, is the player holding?
   wieldedWeapon :: Maybe Item
@@ -48,21 +58,19 @@ data Item = Item {
   isWeapon :: Bool
 } deriving (Eq, Read, Show);
 
--- | For all CharName k, k is the name of a character.
+-- | For all 'CharName' @k@, @k@ is the name of a character.
 data CharName = CharName {
-  -- | For all 'CharName' k, forename k is the forename of a character,
-  -- e.g., "SPINACH".
+  -- | @forename k@ is the forename of a character, e.g., "SPINACH".
   forename :: String,
-  -- | For all 'CharName' k, surname k is the surname of a character,
-  -- e.g., "GRAVYBOAT".
+  -- | @surname k@ is the surname of a character, e.g., "GRAVYBOAT".
   surname :: String,
-  -- | For all 'CharName' k, nickname k is the nickname of a character,
-  -- e.g., "WARREN WESLEY JUNIOR VIII".
+  -- | @nickname k@ is the nickname of a character, e.g., "WARREN WESLEY
+  -- JUNIOR VIII".
   nickname :: String
 } deriving (Eq, Read, Show);
 
--- | State is used to indicate whether a particular character is dead or
--- alive.
+-- | 'State' is used to indicate whether a particular character is dead
+-- or alive.
 data State = Dead | Alive | Win deriving (Eq, Read, Show);
 
 -- | 'Room' represents a room.  No shit.
