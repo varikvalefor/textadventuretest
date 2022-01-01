e65b09c2272b3071e40e4668335604dc87b5bc53
Varik Valefor <varikvalefor@aol.com>
2022-01-01 04:18:13 -0500

    s/drop 1/tail/

--- src/VVXtAdventure/RenegadesOfFunc.hs
+++ src/VVXtAdventure/RenegadesOfFunc.hs
@@ -9,7 +9,7 @@ listInventory = mapM_ (putStrLn . itemName) . inventory;
 
 -- | For all input commands @k@, @daArgz k@ equals the target of @k@.
 daArgz :: String -> String;
-daArgz = unwords . drop 1 . filter (not . isJunk) . words . map toUpper
+daArgz = unwords . tail . filter (not . isJunk) . words . map toUpper
   where
   isJunk :: String -> Bool
   isJunk = flip elem ["TO", "THE", "A", "AN"];
