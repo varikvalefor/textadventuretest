33dc3afc0f2f5d2900a7af6000c7ca8fc23401b9
Varik Valefor <varikvalefor@aol.com>
2022-01-01 07:55:46 -0500

    Line wrapping improves.

--- src/TestAdventure/Messages/Status.hs
+++ src/TestAdventure/Messages/Status.hs
@@ -3,13 +3,20 @@ module TestAdventure.Messages.Status where
 -- | lrTableCrush is output to the terminal if the flimsy-looking living
 -- room table is demolished.
 lrTableCrush :: String;
-lrTableCrush = "Shouting loudly, you smash the flimsy-looking table a la WWE.\nFragments of the table now litter the living room, which was once somewhat clean.";
+lrTableCrush = "Shouting loudly, you smash the flimsy-looking table a \
+               \la WWE.\n\
+               \Fragments of the table now litter the living room, \
+               \which was once somewhat clean.";
 
 -- | lrTableCrushed is output to the terminal if the player attempts to
 -- smash the flimsy-looking living room table... after having already
 -- smashed the living room table.
 lrTableCrushed :: String;
-lrTableCrushed = "You already smashed the table some time ago.  Hell, the thing is still smoking.\nRegardless of this fact, you shout and attempt to smash the deris.  Determining whether or not this attempt is successful is left as an exercise for the reader.";
+lrTableCrushed = "You already smashed the table some time ago.  Hell, \
+                 \the thing is still smoking.\nRegardless of this \
+                 \fact, you shout and attempt to smash the deris.  \
+                 \Determining whether or not this attempt is \
+                 \successful is left as an exercise for the reader.";
 
 -- | lrTableTotesDestroyed is output to the terminal if the player
 -- character attempts to destroy the living room table... after having
@@ -25,21 +32,28 @@ travelSuccess = "You travel successfully.";
 -- | tableFlippedUD is printed to the terminal if the player character
 -- flips the living room table upside-down.
 tableFlippedUD :: String;
-tableFlippedUD = "You grab the card table and throw the thing into the air.  By sheer coincidence, the table manages to land exactly as the table was a few moments ago, with the exception of being upside-down.";
+tableFlippedUD = "You grab the card table and throw the thing into the \
+                 \air.  By sheer coincidence, the table manages to \
+                 \land exactly as the table was a few moments ago, \
+                 \with the exception of being upside-down.";
 
 -- | tableFlippedUU is printed to the terminal if the player character
 -- restores the table to the table's original position, i.e., "de-flips"
 -- the table.
 tableFlippedUU :: String;
-tableFlippedUU = "You restore the table to the table's original position, i.e., you \"de-flip\" the table.";
+tableFlippedUU = "You restore the table to the table's original \
+                 \position, i.e., you \"de-flip\" the table.";
 
 junkLRTableDebris :: String;
-junkLRTableDebris = "You successfully place the rubble into the bin.\nBecause the table is shattered into sharp shards, the bin bag tears a bit.";
+junkLRTableDebris = "You successfully place the rubble into the bin.\n\
+                    \Because the table is shattered into sharp shards, \
+                    \the bin bag tears a bit.";
 
 -- | broomSmashed is printed to the terminal if the player character
 -- smashes the broom.
 broomSmashed :: String;
-broomSmashed = "You smash the broom over your knee, barely avoiding getting some nasty splinters in the process.";
+broomSmashed = "You smash the broom over your knee, barely avoiding \
+               \getting some nasty splinters in the process.";
 
 -- | broomAlreadySmashed is printed to the terminal if the player
 -- character tries to smash the broom... after having already smashed
@@ -61,4 +75,6 @@ mopAlreadySmashed = "The mop is already mutilated.";
 -- | @weaponWielded@ is printed to the terminal if the player
 -- successfully wields a weapon.
 weaponWielded :: String;
-weaponWielded = "You weld the weapon to your hand.\nYou do know that you could have just grabbed the weapon, no?";
+weaponWielded = "You weld the weapon to your hand.\n\
+                \You do know that you could have just grabbed the \
+                \weapon, no?";
